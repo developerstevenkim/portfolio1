0965c7042755eba53501a5cdc355cf77de3e6fad
Steven Kim <stevenkim1989@gmail.com>
2021-11-02 02:00:20 -0700

    reset database

--- Data/Migrations/20211102085928_M1.Designer.cs
+++ Data/Migrations/20211102085928_M1.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DohunKim.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DohunKim.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211102085928_M1")]
+    partial class M1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20211102085928_M1.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DohunKim.Data.Migrations
+{
+    public partial class M1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Occupations_Authors_AuthorName",
+                table: "Occupations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Authors_Authors_AuthorName",
+                table: "Project_Authors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Authors_Projects_ProjectName",
+                table: "Project_Authors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Languages_Languages_LanguageName",
+                table: "Project_Languages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Languages_Projects_ProjectName",
+                table: "Project_Languages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectName",
+                table: "Project_Languages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LanguageName",
+                table: "Project_Languages",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectName",
+                table: "Project_Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorName",
+                table: "Project_Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorName",
+                table: "Occupations",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Occupations_Authors_AuthorName",
+                table: "Occupations",
+                column: "AuthorName",
+                principalTable: "Authors",
+                principalColumn: "AuthorName",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Authors_Authors_AuthorName",
+                table: "Project_Authors",
+                column: "AuthorName",
+                principalTable: "Authors",
+                principalColumn: "AuthorName",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Authors_Projects_ProjectName",
+                table: "Project_Authors",
+                column: "ProjectName",
+                principalTable: "Projects",
+                principalColumn: "ProjectName",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Languages_Languages_LanguageName",
+                table: "Project_Languages",
+                column: "LanguageName",
+                principalTable: "Languages",
+                principalColumn: "LanguageName",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Languages_Projects_ProjectName",
+                table: "Project_Languages",
+                column: "ProjectName",
+                principalTable: "Projects",
+                principalColumn: "ProjectName",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Occupations_Authors_AuthorName",
+                table: "Occupations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Authors_Authors_AuthorName",
+                table: "Project_Authors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Authors_Projects_ProjectName",
+                table: "Project_Authors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Languages_Languages_LanguageName",
+                table: "Project_Languages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Project_Languages_Projects_ProjectName",
+                table: "Project_Languages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectName",
+                table: "Project_Languages",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LanguageName",
+                table: "Project_Languages",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProjectName",
+                table: "Project_Authors",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorName",
+                table: "Project_Authors",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorName",
+                table: "Occupations",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Occupations_Authors_AuthorName",
+                table: "Occupations",
+                column: "AuthorName",
+                principalTable: "Authors",
+                principalColumn: "AuthorName",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Authors_Authors_AuthorName",
+                table: "Project_Authors",
+                column: "AuthorName",
+                principalTable: "Authors",
+                principalColumn: "AuthorName",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Authors_Projects_ProjectName",
+                table: "Project_Authors",
+                column: "ProjectName",
+                principalTable: "Projects",
+                principalColumn: "ProjectName",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Languages_Languages_LanguageName",
+                table: "Project_Languages",
+                column: "LanguageName",
+                principalTable: "Languages",
+                principalColumn: "LanguageName",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Project_Languages_Projects_ProjectName",
+                table: "Project_Languages",
+                column: "ProjectName",
+                principalTable: "Projects",
+                principalColumn: "ProjectName",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
